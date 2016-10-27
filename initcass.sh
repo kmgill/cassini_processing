a74c1def358327fe8ba9f369e2c62c16642d2776
kmgill <kgill@jpl.nasa.gov>
2016-10-27 13:26:29 -0700

    Added a template initcass.sh.

---
+++ initcass.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# NOTE: Edit this with the correct paths!
+
+export PATH=$PATH:/absolute-path-to-cassini_processing-repo/cassini_processing
+export ISISROOT=/absolute-path-to-ISIS3-installation/ISIS/isis
+export ISIS3DATA=/absolute-path-to-ISIS3-installation/ISIS/data
+
+. $ISISROOT/scripts/isis3Startup.sh
