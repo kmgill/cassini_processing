8a42323d2784268d2bf016f66bcb50fd4955d042
kmgill <kgill@jpl.nasa.gov>
2016-11-08 10:01:47 -0800

    Added cub2tiff.py

---
+++ cub2tiff.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import argparse
+
+from isis3 import utils
+
+if __name__ == "__main__":
+
+    try:
+        utils.is_isis3_initialized()
+    except:
+        print "ISIS3 has not been initialized. Please do so. Now."
+        sys.exit(1)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--data", help="Source cube file", required=True, type=str, nargs='+')
+
+    args = parser.parse_args()
+
+    source = args.data
+
+    for file_name in source:
+        if file_name[-3:].upper() != "CUB":
+            print "Not a ISIS cube file file. Skipping '%s'"%file_name
+        else:
+            out_file_tiff = utils.output_tiff_from_label(file_name)
+            utils.export_tiff_grayscale(file_name, out_file_tiff, minimum=None, maximum=None)
--- isis3/utils.py
+++ isis3/utils.py
@@ -42,13 +42,16 @@ def is_isis3_initialized():
 
 
 def get_field_value(lbl_file_name, keyword, objname=None, grpname=None):
-    cmd = ["getkey", "from=%s"%lbl_file_name, "keyword=%s"%keyword]
-    if objname is not None:
-        cmd += ["objname=%s"%objname]
-    if grpname is not None:
-        cmd += ["grpname=%s"%grpname]
-    s = subprocess.check_output(cmd)
-    return s.strip()
+    try:
+        cmd = ["getkey", "from=%s"%lbl_file_name, "keyword=%s"%keyword]
+        if objname is not None:
+            cmd += ["objname=%s"%objname]
+        if grpname is not None:
+            cmd += ["grpname=%s"%grpname]
+        s = subprocess.check_output(cmd)
+        return s.strip()
+    except:
+        return None
 
 def get_product_id(file_name):
     if file_name[-3:].upper() == "LBL":
