add93eccf5cf7436f7d4bb182841644e9f72eb31
Kevin M. Gill <kevin.m.gill@jpl.nasa.gov>
2018-05-12 20:55:30 -0700

    Adding some initial enums for PDS queries

---
+++ sciimg/pds/query.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import requests
+
+
+class QueryFields:
+    mission = "ATLAS_MISSION_NAME"
+    instrument = "ATLAS_INSTRUMENT_NAME"
+    target = "TARGET"
+
+
+class Missions:
+    MARS_ODYSSEY = "2001 mars odyssey"
+    CASSINI = "cassini"
+    CHANDRAYAAN_1 = "chandrayaan-1"
+    CLEMENTINE = "clementine"
+    GALILEO = "galileo"
+    LCROSS = "lcross"
+    LUNAR_ORBITER = "lunar orbiter"
+    LUNAR_RECONNAISSANCE_ORBITER = "lunar reconnaissance orbiter"
+    MAGELLAN = "magellan"
+    MARS_EXPLORATION_ROVER = "mars exploration rover"
+    MARS_GLOBAL_SURVEYOR = "mars global surveyor"
+    MARS_PATHFINDER = "mars pathfinder"
+    MARS_RECONNAISSANCE_ORBITER = "mars reconnaissance orbiter"
+    MARS_SCIENCE_LABORATORY = "mars science laboratory"
+    MESSENGER = "messenger"
+    NEW_HORIZONS = "new horizons"
+    PHOENIX = "phoenix"
+    VIKING_LANDER = "viking lander"
+    VIKING_ORBITER = "viking orbiter"
+    VOYAGER = "voyager"
+
+
+class Spacecraft:
+    MARS_ODYSSEY = "2001 mars odyssey"
+    CARL_SAGAN_MEMORIAL_STATION = "carl sagan memorial station"
+    CASSINI_ORBITER = "cassini orbiter"
+    CHANDRAYAAN_1 = "chandrayaan-1"
+    CLEMENTINE = "clementine"
+    CURIOSITY = "curiosity"
+    GALILEO = "galileo"
+    LCROSS = "lcross"
+    LUNAR_ORBITER_1 = "lunar orbiter 1"
+    LUNAR_ORBITER_2 = "lunar orbiter 2"
+    LUNAR_ORBITER_3 = "lunar orbiter 3"
+    LUNAR_ORBITER_4 = "lunar orbiter 4"
+    LUNAR_ORBITER_5 = "lunar orbiter 5"
+    LUNAR_RECONNAISSANCE_ORBITER = "lunar reconnaissance orbiter"
+    MAGELLAN = "magellan"
+    MARS_GLOBAL_SURVEYOR = "mars global surveyor"
+    MARS_RECONNAISSANCE_ORBITER = "mars reconnaissance orbiter"
+    MESSENGER = "messenger"
+    NEW_HORIZONS = "new horizons"
+    OPPORTUNITY = "opportunity"
+    PHOENIX = "phoenix"
+    SOJOURNER = "sojourner"
+    SPIRIT = "spirit"
+    VIKING_LANDER_1 = "viking lander 1"
+    VIKING_LANDER_2 = "viking lander 2"
+    VIKING_ORBITER_1 = "viking orbiter 1"
+    VIKING_ORBITER_2 = "viking orbiter 2"
+    VOYAGER_1 = "voyager 1"
+    VOYAGER_2 = "voyager 2"
+
+
+
+
+def find(**kwargs):
+    pass
+
+
+def fetch(**kwargs):
+    pass
