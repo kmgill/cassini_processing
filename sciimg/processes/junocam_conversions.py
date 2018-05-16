0b27aab84ef0315fb81fd9a82899001f0d782ddc
Kevin M. Gill <kevin.m.gill@jpl.nasa.gov>
2018-05-16 16:16:39 -0700

    nope

--- sciimg/processes/junocam_conversions.py
+++ sciimg/processes/junocam_conversions.py
@@ -6,7 +6,7 @@ from sciimg.isis3.junocam.utils import json_to_lbl
 
 
 def create_label(output_base, metadata_json_path):
-    output_lbl_path = "%s.LBL"%output_base
+    output_lbl_path = "%s.lbl"%output_base
     lbl_data = json_to_lbl(metadata_json_path)
 
     with open(output_lbl_path, "w") as f:
@@ -15,7 +15,7 @@ def create_label(output_base, metadata_json_path):
 
 def create_pds(output_base, png_file):
     output_cub = "%s.cub"%output_base
-    output_img = "%s.IMG"%output_base
+    output_img = "%s.img"%output_base
 
     importexport.std2isis(png_file, output_cub, mode=importexport.ColorMode.GRAYSCALE)
     importexport.isis2pds(output_cub, output_img, labtype="fixed", bittype="u16bit")
